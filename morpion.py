6f00b895773c29e986d5d9fa81889c4ef2fdb0b9
Antoine Rabany <antoine.rabany@rez-gif.supelec.fr>
2014-04-27 10:46:02 +0200

    Ajout d'un menu de fin

--- morpion.py
+++ morpion.py
@@ -37,96 +37,127 @@ def Somme(morpion):
 	if somme == -3:
 		return -1
 
+	return 0
+
 
 
 fenetre = pygame.display.set_mode((300, 400))
 
+#Chargement des images
 fond = pygame.image.load("background.jpg").convert()
-fenetre.blit(fond, (0,0))
-
-font = pygame.font.Font(None,90)
-
-fenetre.blit(font.render("Menu", 1, (255,0,0)), (0,0))
-fenetre.blit(font.render("1 joueur", 1, (255,0,0)), (0,100))
-fenetre.blit(font.render("2 joueurs", 1, (255,0,0)), (0,200))
-fenetre.blit(font.render("Quitter", 1, (255,0,0)), (0,300))
-
-pygame.display.flip()
-
-menu = 1
-
-#Affichage du menu
-while menu:
-	for event in pygame.event.get():   #On parcours la liste de tous les événements reçus
-		if event.type == QUIT:     #Si un de ces événements est de type QUIT
-			menu = 0     #On arrête la boucle
-		if event.type == MOUSEBUTTONDOWN and event.button == 1:
-			if 100<event.pos[1]<200:
-				continuer = 1
-				joueurs = 1
-				menu = 0
-			if 200<event.pos[1]<300:
-				continuer = 1
-				joueurs = 2
-				menu = 0
-			if 300<event.pos[1]<400:
-				continuer = 0
-				menu = 0
-
-#Chargement et collage du personnage
 croix = pygame.image.load("croix.png").convert_alpha()
 rond = pygame.image.load("rond.png").convert_alpha()
 
 
-pygame.display.flip()
+#Definition de la police de charactère
+font = pygame.font.Font(None,90)
 
-morpion = [[0,0,0],[0,0,0],[0,0,0]]
+menu = 1
+continuer = 1
+jeu = 0
+fin = 0
 
-tour = 0
-
-#Boucle infinie
 while continuer:
 	for event in pygame.event.get():   #On parcours la liste de tous les événements reçus
 		if event.type == QUIT:     #Si un de ces événements est de type QUIT
-			continuer = 0      #On arrête la boucle
+			continuer = 0     #On arrête la boucle
 
-		#Recheche de la cible du clic
-		if event.type == MOUSEBUTTONDOWN and event.button == 1:
-			#On recherche la collone k
-			for k in range(3):
-				if k*100 < event.pos[0] < (k+1)*100:
-					#On recherche la ligne l
-					for l in range(3):
-						if l*100 < event.pos[1] < (l+1)*100:
-							signe = morpion[k][l]
-							#On met la croix ou le rond en fonction du tour
-							if signe == 0:
-								if tour % 2 == 0:
-									morpion[k][l]=1
-								else:
-									morpion[k][l]=-1
-								tour += 1
 
-	if Somme(morpion) == 1:
-		print "Cochon a gagné"
-		morpion = [[0,0,0],[0,0,0],[0,0,0]]
-		tour = 0
-	if Somme(morpion) == -1:
-		print "Vache a gagné"
-		morpion = [[0,0,0],[0,0,0],[0,0,0]]
-		tour = 0
-	if tour == 9:
-		print "egalité"
-		tour = 0
-		morpion = [[0,0,0],[0,0,0],[0,0,0]]
+	#Affichage du menu de debut
+	while menu:
+		for event in pygame.event.get():   #On parcours la liste de tous les événements reçus
+			if event.type == QUIT:     #Si un de ces événements est de type QUIT
+				menu = 0     #On arrête la boucle
+				continuer = 0 
+			if event.type == MOUSEBUTTONDOWN and event.button == 1:
+				if 100<event.pos[1]<200:
+					jeu = 1
+					joueurs = 1
+					menu = 0
+				if 200<event.pos[1]<300:
+					jeu = 1
+					joueurs = 2
+					menu = 0
+				if 300<event.pos[1]<400:
+					continuer = 0
+					menu = 0
 
-	#Affichage du tableau de jeu de morpion
-	fenetre.blit(fond, (0,0))
-	for i,col in enumerate(morpion):
-		for j,ligne in enumerate(col):
-			if ligne == 1:
-				fenetre.blit(croix,(i*100,j*100))
-			if ligne == -1:
-				fenetre.blit(rond,(i*100,j*100) )
-	pygame.display.flip()
+		fenetre.blit(fond, (0,0))
 
+		fenetre.blit(font.render("Menu", 1, (255,0,0)), (0,0))
+		fenetre.blit(font.render("1 joueur", 1, (255,0,0)), (0,100))
+		fenetre.blit(font.render("2 joueurs", 1, (255,0,0)), (0,200))
+		fenetre.blit(font.render("Quitter", 1, (255,0,0)), (0,300))
+
+		pygame.display.flip()		
+
+
+
+	#On initialise les variables du jeu
+	morpion = [[0,0,0],[0,0,0],[0,0,0]]
+	tour = 0
+
+	#Boucle infinie du jeu
+	while jeu:
+		for event in pygame.event.get():   #On parcours la liste de tous les événements reçus
+			if event.type == QUIT:     #Si un de ces événements est de type QUIT
+				jeu = 0      #On arrête la boucle
+				continuer = 0 
+
+			#Recheche de la cible du clic
+			if event.type == MOUSEBUTTONDOWN and event.button == 1:
+				#On recherche la collone k
+				for k in range(3):
+					if k*100 < event.pos[0] < (k+1)*100:
+						#On recherche la ligne l
+						for l in range(3):
+							if l*100 < event.pos[1] < (l+1)*100:
+								signe = morpion[k][l]
+								#On met la croix ou le rond en fonction du tour
+								if signe == 0:
+									if tour % 2 == 0:
+										morpion[k][l]=1
+									else:
+										morpion[k][l]=-1
+									tour += 1
+
+		somme = Somme(morpion)
+		if somme != 0 or tour == 9: # On detecte si le jeu est fini
+			fin = 1 # On enclenche le menu de fin
+			jeu = 0 # On arrete le jeu
+
+		#Affichage du tableau de jeu de morpion
+		fenetre.blit(fond, (0,0))
+		for i,col in enumerate(morpion):
+			for j,ligne in enumerate(col):
+				if ligne == 1:
+					fenetre.blit(croix,(i*100,j*100))
+				if ligne == -1:
+					fenetre.blit(rond,(i*100,j*100) )
+		fenetre.blit(font.render("Morpion", 1, (255,0,0)), (0,300))
+		pygame.display.flip()
+
+	while fin:
+		for event in pygame.event.get():   #On parcours la liste de tous les événements reçus
+			if event.type == QUIT:     #Si un de ces événements est de type QUIT
+				fin = 0      #On arrête la boucle
+				continuer = 0 
+			if event.type == MOUSEBUTTONDOWN and event.button == 1:
+				if 200<event.pos[1]<300:
+					jeu = 1
+					fin = 0
+					menu = 0
+				if 300<event.pos[1]<400:
+					jeu = 0
+					fin = 0
+					menu = 1
+		fenetre.blit(fond, (0,0))
+		if somme == 1:
+			fenetre.blit(font.render("Cochon a gagné", 1, (255,0,0)), (0,0))
+		if somme == -1:
+			fenetre.blit(font.render("Vache a gagné", 1, (255,0,0)), (0,0))
+		if tour == 9:
+			fenetre.blit(font.render("Egalité", 1, (255,0,0)), (0,0))
+		fenetre.blit(font.render("Rejouer", 1, (255,0,0)), (0,200))
+		fenetre.blit(font.render("Menu", 1, (255,0,0)), (0,300))
+		pygame.display.flip()
